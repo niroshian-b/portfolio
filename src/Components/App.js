2169ca5f0b3bb229cd67a99f8884d551a594f961
Niroshian Balasubramaniam <niroshian.b@gmail.com>
2021-05-10 09:40:36 -0400

    starter files added

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from '../Globals/GlobalStyles';
+import Header from './Header';
+
+const App = () => {
+	return (
+		<div>
+			<GlobalStyles />
+			<Header />
+		</div>
+	);
+};
+
+export default App;
