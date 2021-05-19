ad689c00a6dc2284d6239447a537ec6bbe3c0850
Niroshian Balasubramaniam <niroshian.b@gmail.com>
2021-05-19 15:29:54 -0400

    setting up heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv').config();
+const path = require('path');
 const cors = require('cors');
 const router = express.Router();
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
@@ -71,4 +73,7 @@ router.post('/email/send', (req, res) => {
 	});
 });
 
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 app.listen(port, () => console.log(`listening on port ${port}`));
